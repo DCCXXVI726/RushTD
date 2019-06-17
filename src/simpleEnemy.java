b6188711c880ccf4fa849d0f9e1150259924ffe3
Agnitt <imakbel@gmail.com>
2019-06-17 11:32:28 +0300

    Enemy abstract v 0.1

--- src/simpleEnemy.java
+++ src/simpleEnemy.java
@@ -1,5 +1,10 @@
 public class simpleEnemy extends Enemy {
-
+   simpleEnemy (double x, double y) {
+        hp = 1000000;
+        speed = -1000000;
+        this.x = x;
+        this.y = y;
+    }
     @Override
     public int takeDamage(int damage) {
         return hp - damage;
